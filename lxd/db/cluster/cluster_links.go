bd7ea07bb7eb9b07490540f62338da297ea78091
Kadin Sayani <kadin.sayani@canonical.com>
2025-01-17 13:45:46 -0700

    lxd/db/cluster: Add cluster link db model
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

---
+++ lxd/db/cluster/cluster_links.go
@@ -0,0 +1,178 @@
+package cluster
+
+import (
+	"context"
+	"database/sql"
+	"database/sql/driver"
+	"fmt"
+
+	"github.com/canonical/lxd/lxd/db/query"
+	"github.com/canonical/lxd/shared/api"
+)
+
+// ClusterLinkType represents the type of a cluster link stored as a string in the database.
+//
+// This type implements the [sql.Scanner] and [driver.Value] interfaces to automatically handle conversion between API constants and their int64 representation in the database.
+// When reading from the database, int64 values are converted back to their constant type.
+// When writing to the database, API constants are converted to their int64 representation.
+type ClusterLinkType string
+
+const (
+	clusterLinkTypeBidirectional int64 = 0
+)
+
+// ClusterLinkRow represents a single row of the cluster_links table.
+// db:model cluster_links
+type ClusterLinkRow struct {
+	ID          int64           `db:"id"`
+	IdentityID  int64           `db:"identity_id"`
+	Name        string          `db:"name"`
+	Description string          `db:"description"`
+	Type        ClusterLinkType `db:"type"`
+}
+
+// APIName implements [query.APINamer] for API friendly error messages.
+func (ClusterLinkRow) APIName() string {
+	return "Cluster link"
+}
+
+// ScanInteger implements [query.IntegerScanner] for [ClusterLinkType].
+func (c *ClusterLinkType) ScanInteger(clusterLinkTypeCode int64) error {
+	switch clusterLinkTypeCode {
+	case clusterLinkTypeBidirectional:
+		*c = api.ClusterLinkTypeBidirectional
+	default:
+		return fmt.Errorf("Unknown cluster link type %d", clusterLinkTypeCode)
+	}
+
+	return nil
+}
+
+// Scan implements [sql.Scanner] for [ClusterLinkType]. This converts the database integer value back into the correct API constant or returns an error.
+func (c *ClusterLinkType) Scan(value any) error {
+	return query.ScanValue(value, c, false)
+}
+
+// Value implements [driver.Value] for [ClusterLinkType]. This converts the API constant into its integer database representation or throws an error.
+func (c ClusterLinkType) Value() (driver.Value, error) {
+	switch c {
+	case api.ClusterLinkTypeBidirectional:
+		return clusterLinkTypeBidirectional, nil
+	}
+
+	return nil, fmt.Errorf("Invalid cluster link type %q", c)
+}
+
+// ToAPI converts the database [ClusterLinkRow] struct to API type [api.ClusterLink].
+func (r *ClusterLinkRow) ToAPI(ctx context.Context, tx *sql.Tx) (*api.ClusterLink, error) {
+	config, err := GetClusterLinkConfig(ctx, tx, r.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &api.ClusterLink{
+		Name:        r.Name,
+		Description: r.Description,
+		Type:        string(r.Type),
+		Config:      config,
+	}, nil
+}
+
+// GetClusterLinks returns all cluster links.
+func GetClusterLinks(ctx context.Context, tx *sql.Tx) ([]ClusterLinkRow, error) {
+	return query.Select[ClusterLinkRow](ctx, tx, "ORDER BY name")
+}
+
+// GetClusterLink returns the cluster link with the given name.
+func GetClusterLink(ctx context.Context, tx *sql.Tx, name string) (*ClusterLinkRow, error) {
+	link, err := query.SelectOne[ClusterLinkRow](ctx, tx, "WHERE name = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("Failed loading cluster link: %w", err)
+	}
+
+	return link, nil
+}
+
+// CreateClusterLink adds a new cluster link to the database.
+func CreateClusterLink(ctx context.Context, tx *sql.Tx, object ClusterLinkRow) (int64, error) {
+	return query.Create(ctx, tx, object)
+}
+
+// UpdateClusterLink updates the cluster link matching the given name.
+func UpdateClusterLink(ctx context.Context, tx *sql.Tx, name string, object ClusterLinkRow) error {
+	return query.UpdateOne(ctx, tx, object, "WHERE name = ?", name)
+}
+
+// DeleteClusterLink deletes the cluster link with the given name.
+func DeleteClusterLink(ctx context.Context, tx *sql.Tx, name string) error {
+	return query.DeleteOne[ClusterLinkRow, *ClusterLinkRow](ctx, tx, "WHERE name = ?", name)
+}
+
+// RenameClusterLink renames the cluster link with the given name.
+func RenameClusterLink(ctx context.Context, tx *sql.Tx, name string, to string) error {
+	link, err := GetClusterLink(ctx, tx, name)
+	if err != nil {
+		return err
+	}
+
+	link.Name = to
+	return query.UpdateByPrimaryKey(ctx, tx, *link)
+}
+
+// GetClusterLinkConfig returns the config for the cluster link with the given ID.
+func GetClusterLinkConfig(ctx context.Context, tx *sql.Tx, clusterLinkID int64) (map[string]string, error) {
+	q := `SELECT key, value FROM cluster_links_config WHERE cluster_link_id=?`
+
+	config := map[string]string{}
+	return config, query.Scan(ctx, tx, q, func(scan func(dest ...any) error) error {
+		var key, value string
+
+		err := scan(&key, &value)
+		if err != nil {
+			return err
+		}
+
+		_, found := config[key]
+		if found {
+			return fmt.Errorf("Duplicate config row found for key %q for cluster link ID %d", key, clusterLinkID)
+		}
+
+		config[key] = value
+
+		return nil
+	}, clusterLinkID)
+}
+
+// CreateClusterLinkConfig creates config for a new cluster link with the given ID.
+func CreateClusterLinkConfig(ctx context.Context, tx *sql.Tx, clusterLinkID int64, config map[string]string) error {
+	str := "INSERT INTO cluster_links_config (cluster_link_id, key, value) VALUES(?, ?, ?)"
+	stmt, err := tx.Prepare(str)
+	if err != nil {
+		return err
+	}
+
+	defer func() { _ = stmt.Close() }()
+
+	for k, v := range config {
+		if v == "" {
+			continue
+		}
+
+		_, err = stmt.ExecContext(ctx, clusterLinkID, k, v)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// UpdateClusterLinkConfig updates the cluster link with the given ID, setting its config.
+func UpdateClusterLinkConfig(ctx context.Context, tx *sql.Tx, clusterLinkID int64, config map[string]string) error {
+	_, err := tx.ExecContext(ctx, "DELETE FROM cluster_links_config WHERE cluster_link_id=?", clusterLinkID)
+	if err != nil {
+		return err
+	}
+
+	return CreateClusterLinkConfig(ctx, tx, clusterLinkID, config)
+}
