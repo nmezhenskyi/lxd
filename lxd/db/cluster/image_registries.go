9a3b904546037fe4d93bea66416fadec2338af36
Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>
2026-03-18 14:06:13 -0400

    lxd/db/cluster: Add image registries config database functions
    Signed-off-by: Nikita Mezhenskyi <nikita.mezhenskyi@canonical.com>

--- lxd/db/cluster/image_registries.go
+++ lxd/db/cluster/image_registries.go
@@ -1,6 +1,8 @@
 package cluster
 
 import (
+	"context"
+	"database/sql"
 	"database/sql/driver"
 	"fmt"
 
@@ -92,4 +94,117 @@ func (p ImageRegistryProtocol) Value() (driver.Value, error) {
 	}
 
 	return nil, fmt.Errorf("Invalid image registry protocol %q", p)
+}
+
+// GetImageRegistryConfig returns associated config of the existing image registry with the given name.
+func GetImageRegistryConfig(ctx context.Context, tx *sql.Tx, name string) (map[string]string, error) {
+	registry, err := GetImageRegistry(ctx, tx, name)
+	if err != nil {
+		return nil, fmt.Errorf("Failed loading image registry: %w", err)
+	}
+
+	config, err := imageRegistryConfigGet(ctx, tx, registry.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+// CreateImageRegistryConfig creates config for a new image registry with the given name.
+func CreateImageRegistryConfig(ctx context.Context, tx *sql.Tx, name string, config map[string]string) error {
+	id, err := GetImageRegistryID(ctx, tx, name)
+	if err != nil {
+		return err
+	}
+
+	err = imageRegistryConfigAdd(ctx, tx, id, config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateImageRegistryConfig updates the existing image registry with the given name by setting its config.
+func UpdateImageRegistryConfig(ctx context.Context, tx *sql.Tx, name string, config map[string]string) error {
+	id, err := GetImageRegistryID(ctx, tx, name)
+	if err != nil {
+		return err
+	}
+
+	// Clear the config.
+	err = imageRegistryConfigDelete(ctx, tx, id)
+	if err != nil {
+		return err
+	}
+
+	// Apply the new config.
+	err = imageRegistryConfigAdd(ctx, tx, id, config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// imageRegistryConfigAdd adds config to the image registry with the given ID.
+func imageRegistryConfigAdd(ctx context.Context, tx *sql.Tx, registryID int64, config map[string]string) error {
+	stmt, err := tx.Prepare("INSERT INTO image_registries_config (image_registry_id, key, value) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+
+	defer func() { _ = stmt.Close() }()
+
+	for k, v := range config {
+		if v == "" {
+			continue
+		}
+
+		_, err = stmt.ExecContext(ctx, registryID, k, v)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// imageRegistryConfigDelete removes all config from the image registry with the given ID.
+func imageRegistryConfigDelete(ctx context.Context, tx *sql.Tx, registryID int64) error {
+	_, err := tx.ExecContext(ctx, "DELETE FROM image_registries_config WHERE image_registry_id = ?", registryID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// imageRegistryConfigGet is a helper to return associated config of the existing image registry with the given ID.
+func imageRegistryConfigGet(ctx context.Context, tx *sql.Tx, registryID int64) (map[string]string, error) {
+	stmt := `SELECT key, value FROM image_registries_config WHERE image_registry_id = ?`
+
+	config := map[string]string{}
+	err := query.Scan(ctx, tx, stmt, func(scan func(dest ...any) error) error {
+		var key, value string
+
+		err := scan(&key, &value)
+		if err != nil {
+			return err
+		}
+
+		_, alreadySet := config[key]
+		if alreadySet {
+			return fmt.Errorf("Duplicate config row found for key %q for image registry ID %d", key, registryID)
+		}
+
+		config[key] = value
+		return nil
+	}, registryID)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
 }
